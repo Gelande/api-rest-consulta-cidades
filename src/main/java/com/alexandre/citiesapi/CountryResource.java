d47c4249fd3f4d1f211a0a5db2966f79edcc4ebc
Gelande <agelande@gmail.com>
2021-09-05 22:48:10 -0300

    Criado o recurso - API/DB

---
+++ src/main/java/com/alexandre/citiesapi/CountryResource.java
@@ -0,0 +1,16 @@
+package com.alexandre.citiesapi;
+
+import com.alexandre.citiesapi.countries.Country;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryResource {
+
+    @GetMapping
+    public List<Country> countries() {
+        return ;
+    }
+}
---
+++ src/main/java/com/alexandre/citiesapi/countries/Country.java
@@ -0,0 +1,7 @@
+package com.alexandre.citiesapi.countries;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Country {
+}
